d28b08cc935641e75d5ad71ded6b007f29de63a9
Jeffrey <Jeffrey@simpleyapp.com>
2020-03-28 11:25:37 -0400

    Initial Commit

---
+++ Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screen
+//
+//  Created by Jeffrey  on 3/28/20.
+//  Copyright © 2020 BMCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
